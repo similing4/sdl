1374a1dd263be9b522a7d2c46bad5a01a7de4e79
similing4 <similing4@163.com>
2021-10-24 22:34:32 +0800

    同步

--- main.cpp
+++ main.cpp
@@ -9,23 +9,29 @@ extern "C" {
 }
 constexpr int RenderTextureMaxLength = 655360;
 constexpr int SpriteMaxLength = 65536;
-typedef struct {
+struct Texture {
     SDL_Texture* texture = NULL;
-}Texture;
-typedef struct {
+    int w;
+    int h;
+};
+struct Sprite {
     int zIndex;
-    int textureListLen;
-    int* textureList; //动画图片列表，需要malloc free
-    int delay; //刷新频率（毫秒），即1000fps
-    int currentTextureListIndex = 0; //当前播放帧
-}Sprite;
+    int x;
+    int y;
+    int w;
+    int h;
+    int xd = 1; //横向分割1个
+    int yd = 1; //纵向分割1个
+    int textureIndex; //对应的纹理
+    int currentPlayIndex = 0; //当前播放帧(从左到右从上到下)
+};
 
 SDL_Window* win = NULL;
 SDL_Renderer* ren = NULL;
-Sprite sprites[SpriteMaxLength]; //动画列表
-bool sprites_pos[SpriteMaxLength] = { false }; //0.0625MB内存用于记录这块内存是否被使用
-Texture render_texture[RenderTextureMaxLength]; //2.5MB内存可容纳655,360张图片
-bool render_texture_pos[RenderTextureMaxLength] = { false }; //0.625MB内存用于记录这块内存是否被使用
+Sprite sprites[SpriteMaxLength]; //精灵
+bool sprites_pos[SpriteMaxLength] = { false }; //精灵是否被使用
+Texture render_texture[RenderTextureMaxLength]; //图片
+bool render_texture_pos[RenderTextureMaxLength] = { false }; //图片是否被使用
 
 
 int errorAndExit(const char* err) {
@@ -43,10 +49,14 @@ int addTexture(const char* imagePath) {
         return -1;
     }
     SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, image);
+    int w = image->w;
+    int h = image->h;
     SDL_FreeSurface(image);
     for (int i = 0; i < RenderTextureMaxLength; i++) {
         if (!render_texture_pos[i]) {
             render_texture[i].texture = tex;
+            render_texture[i].w = w;
+            render_texture[i].h = h;
             render_texture_pos[i] = true;
             return i;
         }
@@ -55,11 +65,7 @@ int addTexture(const char* imagePath) {
     return -1;
 }
 
-int addSprite(const char* imagePath) {
-
-}
-
-bool deleteTexture(int index) {
+void deleteTexture(int index) {
     if (render_texture_pos[index]) {
         SDL_DestroyTexture(render_texture[index].texture);
         render_texture[index].texture = NULL;
@@ -67,13 +73,29 @@ bool deleteTexture(int index) {
     }
 }
 
-bool deleteSprite(int index) {
+int addSprite(const char* imagePath, int x, int y, int w, int h, int xd, int yd, int zIndex, int currentPlayIndex) {
+    int ret = addTexture(imagePath);
+    if (ret == -1)
+        return -1;
+    for (int i = 0; i < SpriteMaxLength; i++) {
+        if (!sprites_pos[i]) {
+            sprites[i].textureIndex = ret;
+            sprites[i].xd = xd;
+            sprites[i].yd = yd;
+            sprites[i].zIndex = zIndex;
+            sprites[i].currentPlayIndex = currentPlayIndex;
+            sprites_pos[i] = true;
+            return i;
+        }
+    }
+    deleteTexture(ret);
+    return -1;
+}
+
+void deleteSprite(int index) {
     if (sprites_pos[index]) {
-        for (int i = 0; i < sprites[index].textureListLen; i++)
-            deleteTexture(sprites[index].textureList[i]);
-        free(sprites[index].textureList);
-        sprites[index].textureListLen = 0;
         sprites_pos[index] = false;
+        deleteTexture(sprites[index].textureIndex);
     }
 }
 
@@ -85,20 +107,33 @@ void cleanTextureAndSprite() {
 }
 
 int main(int argc, char* argv[]) {
-    FILE* rf = freopen("stdout.log", "w", stdout);
+    FILE* rf;
+    freopen_s(&rf, "stdout.log", "w", stdout);
     if (SDL_Init(SDL_INIT_VIDEO))
         return errorAndExit("SDL_Init Error: ");
-    win = SDL_CreateWindow("魔塔", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 17 * 32, 11 * 32, SDL_WINDOW_SHOWN);
+    win = SDL_CreateWindow("Mota", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 17 * 32, 11 * 32, SDL_WINDOW_SHOWN);
     if (win == nullptr)
         return errorAndExit("SDL_CreateWindow Error: ");
     ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
     if (ren == nullptr)
         return errorAndExit("SDL_CreateRender Error: ");
 
-
-    for (int i = 0; i < 3; ++i) {
+    SDL_Rect sourceRect;
+    SDL_Rect destRect;
+    while(true) {
         SDL_RenderClear(ren);
-        SDL_RenderCopy(ren, tex, NULL, NULL);
+        for (int i = 0; i < SpriteMaxLength; i++)
+            if (sprites_pos[i]) {
+                sourceRect.w = (render_texture[sprites[i].textureIndex].w / sprites[i].xd);
+                sourceRect.h = (render_texture[sprites[i].textureIndex].h / sprites[i].yd);
+                sourceRect.x = (sprites[i].currentPlayIndex % sprites[i].xd) * sourceRect.w;
+                sourceRect.y = (sprites[i].currentPlayIndex / sprites[i].xd) * sourceRect.h;
+                destRect.x = sprites[i].x;
+                destRect.y = sprites[i].y;
+                destRect.w = sprites[i].w;
+                destRect.h = sprites[i].h;
+                SDL_RenderCopy(ren, render_texture[sprites[i].textureIndex].texture, &sourceRect, &destRect);
+            }
         SDL_RenderPresent(ren);
         SDL_Delay(1000);
     }
